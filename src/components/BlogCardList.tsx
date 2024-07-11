1174d669d08d01599f8caa04520fe9d331006f1f
CoffeeKiller <18320210361@163.com>
2024-07-11 17:23:22 +0800

    pref

--- src/components/BlogCardList.tsx
+++ src/components/BlogCardList.tsx
@@ -5,35 +5,50 @@ import type { CollectionEntry } from "astro:content"
 import { getQueryString, changeURLArg } from "@utils/urlUtil"
 import { getPagination } from "@utils/postsUtil"
 import { iLog } from '@utils/iLogUtil'
+import { allMdArr } from "@utils/astroUtil"
+
+function getPosts(): CollectionEntry<"blog">[] {
+    return allMdArr.filter(md => md.data.class === '技术')
+				.sort((p1, p2) => p2.data.pubDate.getTime() - p1.data.pubDate.getTime())
+}
 
 // 评论组件
 const id = 'blogs-list'
 
-const BlogCardList = ({ posts }: { posts: CollectionEntry<"blog">[] }) => {
+const BlogCardList = () => {
     
     const [mounted, setMounted] = React.useState(false)
     const [page, setPage] = React.useState("1")
-    const [collection, setCollection] = React.useState(posts)
+    const [collection, setCollection] = React.useState(getPosts())
+    const [totalPages, setTotalPages] = React.useState(1)
     
     React.useEffect(() => {
         setMounted(true)
         setPage(getQueryString('page') === '' ? "1" : getQueryString('page'))
         if (page === "1") {
-            setCollection(
-                getPagination({
-                    posts: posts, page: 1, isIndex: false
-                }).paginatedPosts
-            )
+            const { totalPages, paginatedPosts } = getPagination({
+                posts: getPosts(), page: 1, isIndex: false
+            })
+            setCollection(paginatedPosts)
+            setTotalPages(totalPages)
+        }
+        if (Number(page) > totalPages) {
+            setPage(totalPages.toString())
         }
     }, [])
 
     React.useEffect(() => {
         if (page !== "1") {
-            setCollection(
-                getPagination({
-                    posts: posts, page: page, isIndex: false
-                }).paginatedPosts
-            )
+
+            if (Number(page) > totalPages) {
+                setPage(totalPages.toString())
+                return 
+            }
+
+            const { paginatedPosts } = getPagination({
+                posts: getPosts(), page: page, isIndex: false
+            })
+            setCollection(paginatedPosts)
             const _url = changeURLArg(window.location.href, 'page', page)
             window.history.pushState({}, '', _url)
         }
@@ -49,6 +64,7 @@ const BlogCardList = ({ posts }: { posts: CollectionEntry<"blog">[] }) => {
                         return (<BlogCard post={md} key={md.slug}/>)
                     })
             }
+            <div>总页数: { totalPages }, 当前页码: { page }</div>
         </div>
     )
 }
