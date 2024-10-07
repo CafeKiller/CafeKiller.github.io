253bc3b043039d984504252338a5fc7987ebe29d
CoffeeKiller <18320210361@163.com>
2024-10-07 23:15:34 +0800

    refactor: update searchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,9 @@
-import * as React from 'react'
+import { useEffect, useRef, useState } from 'react'
+
+import { getQueryString, changeURLArg } from "@utils/urlUtil"
 
 import '@styles/global.min.css'
+import '@styles/searchBar.min.css'
 
 
 // 文章搜索组件
@@ -8,13 +11,42 @@ const id = 'search-bar'
 
 const SearchBar = () => {
 
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [inputVal, setInputVal] = useState<string>("");
+
+    // 获取 input 的当前值
     const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
-        console.log(e.currentTarget.value)
+        setInputVal(e.currentTarget.value)
     }
+
+    useEffect(() => {
+        const _query = getQueryString('q')
+        if (_query) {
+            setInputVal(_query)
+            inputRef.current?.focus()
+        }
+    },[])
+
+    useEffect(() => {
+        
+        // 同步至 URL 参数
+        if(inputVal.length > 0) {
+            const _url = changeURLArg(window.location.href, 'q', inputVal);
+            window.history.pushState({}, '', _url)
+        } else {
+            history.replaceState(history.state, "", window.location.pathname);
+        }
+    })
 
     return (
         <div className='search-bar'>
-            <input id={id} type="text" onChange={handleInputChange} placeholder="搜索文章" />
+            <input 
+                id={id} 
+                type="text"  
+                ref = {inputRef}
+                value = {inputVal}
+                onChange={handleInputChange} 
+                placeholder="请输入你需要搜索文章的关键字" />
         </div>
     )
 }
