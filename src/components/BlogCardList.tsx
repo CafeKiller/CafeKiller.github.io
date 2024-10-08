b04380fd22d2f00abe294223ade1a13f58d37807
CoffeeKiller <18320210361@163.com>
2024-10-08 21:32:42 +0800

    refactor: update BlogCardList component

--- src/components/BlogCardList.tsx
+++ src/components/BlogCardList.tsx
@@ -5,23 +5,22 @@ import Paginator from '@components/Paginator'
 import type { CollectionEntry } from "astro:content"
 import { getQueryString, changeURLArg } from "@utils/urlUtil"
 import { getPagination } from "@utils/postsUtil"
-import { allMdArr } from "@utils/astroUtil"
 
 import '@styles/global.min.css'
 
-function getPosts(): CollectionEntry<"blog">[] {
-    return allMdArr.filter(md => md.data.class === '技术')
+function getPosts(posts: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] {
+    return posts.filter(md => md.data.class === '技术')
 				.sort((p1, p2) => p2.data.pubDate.getTime() - p1.data.pubDate.getTime())
 }
 
 // 评论组件
 const id = 'blogs-list'
 
-const BlogCardList = () => {
+const BlogCardList = ({ posts } : {posts: CollectionEntry<"blog">[]}) => {
     
     const [mounted, setMounted] = React.useState(false)
     const [page, setPage] = React.useState("1")
-    const [collection, setCollection] = React.useState(getPosts())
+    const [collection, setCollection] = React.useState(getPosts(posts))
     const [totalPages, setTotalPages] = React.useState(1)
     
     React.useEffect(() => {
@@ -31,7 +30,7 @@ const BlogCardList = () => {
         setPage(_query)
         if (page === "1") {
             const { totalPages, paginatedPosts } = getPagination({
-                posts: getPosts(), page: 1, isIndex: false
+                posts: getPosts(posts), page: 1, isIndex: false
             })
             setCollection(paginatedPosts)
             setTotalPages(totalPages)
@@ -48,7 +47,7 @@ const BlogCardList = () => {
                 return 
             }
             const { paginatedPosts } = getPagination({
-                posts: getPosts(), page: page, isIndex: false
+                posts: getPosts(posts), page: page, isIndex: false
             })
             setCollection(paginatedPosts)
             const _url = changeURLArg(window.location.href, 'page', page)
