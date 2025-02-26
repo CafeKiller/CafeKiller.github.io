e71ccd0fdc98b11d4bb49cf308b8605f38a76608
CoffeeKiller <18320210361@163.com>
2025-02-26 20:22:12 +0800

    refactor: update ImageSwiper

--- src/components/ImageSwiper/index.tsx
+++ src/components/ImageSwiper/index.tsx
@@ -7,23 +7,31 @@ const id = 'image-swiper'
 
 const ImageSwiper = ({images} : { images: imageType[] }) => {
     
-    const [mounted, setMounted] = React.useState(false);
-    const [desc, setDesc] = React.useState('');
-    // const [currentImage, setCurrentImage] = React.useState<imageType>();
     const [currentIdx, setCurrentIdx] = React.useState(0);
 
+    const prveImage = () => {
+        currentIdx == 0 ? setCurrentIdx(images.length - 1) : setCurrentIdx(currentIdx - 1);
+    }
+    const nextImage = () => {
+        currentIdx == images.length - 1 ? setCurrentIdx(0) : setCurrentIdx(currentIdx + 1);
+    }
+
     React.useEffect(() => {
-        setMounted(true)
-        // setCurrentImage(images[currentIdx]);
-    }, [])
+    }, [currentIdx])
 
     return (
         images.length > 0 && (
             <div className='swiper-cont'>
+                <p className='tips'>点击图片可以查看原图</p>
                 <div className="swiper-imgbox">
-                    <img src={images[currentIdx]?.src} alt={images[currentIdx]?.title} />
+                    <img 
+                        src={images[currentIdx]?.src} 
+                        alt={images[currentIdx]?.title} 
+                        onClick={ () => { window.open(images[currentIdx]?.src, "_blank")} }/>
                 </div>
-                <div className="image-title">{currentIdx+1} - {images[currentIdx]?.title}</div>
+                <div className="image-title">{ images[currentIdx]?.title }</div>
+                <button className='swiper-btn prve' onClick={ () => { prveImage() } }> ← </button>
+                <button className='swiper-btn next' onClick={ () => { nextImage() } }> → </button>
             </div>
         )
         
