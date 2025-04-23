edd43e15446d292a5db8884ab37151519cc29900
CoffeeKiller <18320210361@163.com>
2025-04-23 23:03:50 +0800

    style: update cover image

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const SITE: Site = {
     author: 'Coffee_Killer',
     desc: '浩瀚星空为我梦, 星辰大海任我游, 这是我的一方小天地, 隐匿在数据海洋的一处角落, 此处珍藏着我的成长与故事, 愿我能不断创作与进步, 成为一个有趣的人.',
     title: ' - CafeBlogs',
-    ogImage: 'https://s21.ax1x.com/2024/07/01/pkcx1nx.png',
+    ogImage: '//testingcf.jsdelivr.net/gh/cafekiller/cafe.cdn/images/header.jpg',
     lightAndDarkMode: true,
     postPerPage: import.meta.env ? 4 : 5,
     webPrefix: '',
