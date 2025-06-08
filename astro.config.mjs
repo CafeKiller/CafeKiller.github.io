c904667917cad519ed9806d1f08c2ec0ecb9508d
CoffeeKiller <18320210361@163.com>
2025-06-08 19:49:17 +0800

    refactor: remove tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,26 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    host: true,
-    port: 4321
-  },
-  site: 'https://CafeKiller.github.io',
-  redirects: {
-    '/posts': '/tags/posts',
-    '/articles': '/tags/articles',
-    '/notes': '/notes/1',
-    '/blogs/2025': '/blogs/#2025',
-    '/blogs/2024': '/blogs/#2024',
-    '/blogs/2023': '/blogs/#2023',
-    '/blogs/2022': '/blogs/#2022'
-  },
-  integrations: [react(), tailwind()],
-  devToolbar: {
-    enabled: false
-  }
+    server: {
+        host: true,
+        port: 4321
+    },
+    site: 'https://CafeKiller.github.io',
+    redirects: {
+        '/posts': '/tags/posts',
+        '/articles': '/tags/articles',
+        '/notes': '/notes/1',
+        '/blogs/2025': '/blogs/#2025',
+        '/blogs/2024': '/blogs/#2024',
+        '/blogs/2023': '/blogs/#2023',
+        '/blogs/2022': '/blogs/#2022'
+    },
+    integrations: [
+        react()
+    ],
+    devToolbar: {
+        enabled: false
+    }
 });
